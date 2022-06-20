e62fa30861facfe53c4871a83c510268d45055c1
Zammon_V <87796940+Zammon@users.noreply.github.com>
2022-06-20 18:19:12 +0700

    Update! [Navbar + DropDown + Coin(Hover)]
    - Fixed a bug with dropdowns.
    ( But have not added the part that is clicked outside the area of the button to close it. )
    
    - The size of the icons in the dropdown has now been adjusted to fit.
    
    - Fixed the coin hover so that it can now show under the coin icon but not complete responsive are left a bit.

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,5 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
+import DropdownUser from './DropdownUser'
 import vector from './../imagins/Vector.png';
 import coins from './../imagins/Coins.png';
 import notification from './../imagins/Notification.png';
@@ -6,11 +7,42 @@ import cloud from './../imagins/Cloud.png';
 import userprofile from './../imagins/Profile_Peotal.png';
 import logo from './../imagins/Logo.png'
 import './../css/Navbar.css';
-import './../css/Userbox.css'
+import './../css/DropdownUser.css'
+import Wallet from "./Wallet";
 
+{/*Navbar Component*/} 
 function Navbar() {
-    const num = '---';
+    const num = '$1500';
+    let [opendrop, setOpendrop] = useState(false);
     
+    let [inDropdown,setIndropdown] = useState('');
+    let [actives, setActives] = useState('');
+    
+    let [walletdrop, setWalletdrop] = useState('');
+    let [walletsCoin, setwalletsCoin] = useState('');
+    
+    function clickDropdown(bu) {
+         if(bu){
+                setIndropdown(<DropdownUser />);
+                setActives('-active');
+                setOpendrop(true);
+         } else {
+                setIndropdown('');
+                setActives('');
+                setOpendrop(false);
+         }
+        
+    }
+
+    function hoverDropdown(bu){
+        if(bu){
+            setwalletsCoin(<Wallet />);
+            setWalletdrop(bu);
+        } else {
+            setwalletsCoin('');
+            setWalletdrop(false);
+        }
+    }
 
     return(
     <>
@@ -27,16 +59,18 @@ function Navbar() {
         </div>
         <div className='item-right'>
             <div className="box-coins">
-                <img className="coins-Nav" src={coins} />
+                <img className="coins-Nav" src={coins} onMouseEnter={()=>hoverDropdown(true)} onMouseLeave={()=>hoverDropdown(false)} />
                 <div className="div-coins mar-top" style={{color:"white"}}> {`:${num}`} </div>
             </div>
             <img className="cloud-Nav" src={cloud} />
             <img className="notification-Nav item-child" src={notification} />
-            <img className="profile-user" src={userprofile} />
-        </div> 
+            <img className={`profile-user${actives}`} src={userprofile} onClick={()=>clickDropdown(!opendrop)}/>
+            {walletsCoin}
+            {inDropdown}
+        </div>
     </div>
     </>
     );
-} 
+}
 
 export default Navbar 
